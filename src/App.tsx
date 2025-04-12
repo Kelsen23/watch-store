9bd2d33846c2460ade930f68f4d3b361503379b2
Kelsen23 <nikolozigoreziani@gmail.com>
2025-04-12 14:10:00 +0400

    Add filtration functionality to App for filtering to fully work

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 import ProductCard from "./components/ProductCard.js";
-import Sidebar from "./components/Sidebar"
+import Sidebar from "./components/Sidebar";
 import { data } from "./db/data";
+import { useFilterStore } from "./store.js";
+
+interface Product {
+  id: string;
+  img: Record<string, string>;
+  title: string;
+  price: number;
+  company: string;
+  country: string;
+}
 
 const App = () => {
+  const { selectedCountries, selectedColors, selectedPriceRange } =
+    useFilterStore();
+
+  const filteredData = data.filter((item: Product) => {
+    const matchesColors =
+      selectedColors.length === 0 ||
+      Object.keys(item.img).some((color) => selectedColors.includes(color));
+
+    const matchesCountry =
+      selectedCountries.length === 0 ||
+      selectedCountries.includes(item.country);
+
+    const matchesPrice = selectedPriceRange
+      ? item.price >= selectedPriceRange.min &&
+        item.price <= selectedPriceRange.max
+      : true;
+
+    return matchesColors && matchesCountry && matchesPrice;
+  });
+
   return (
     <div>
       <Sidebar />
       <div className="p-4 flex flex-wrap justify-center items-center">
-        {data.map((product: any) => (
+        {filteredData.map((product: any) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
