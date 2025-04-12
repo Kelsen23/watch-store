328a3623973df46aa793f70468a132ad0fd55764
Kelsen23 <nikolozigoreziani@gmail.com>
2025-04-12 12:26:04 +0400

    Create ProductCard component logic, UI and implement it in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import ProductCard from "./components/ProductCard.js";
 import Sidebar from "./components/Sidebar"
+import { data } from "./db/data";
 
 const App = () => {
   return (
     <div>
       <Sidebar />
+      <div className="p-4 flex flex-wrap justify-center items-center">
+        {data.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   )
 }
